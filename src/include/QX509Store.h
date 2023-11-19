79b82af7d9332eb6c34363f9a697bf9eccf0185d
BrutalWizard <bru74lw1z4rd@gmail.com>
2023-11-19 18:47:53 +0300

    Updated licence.

--- src/include/QX509Store.h
+++ src/include/QX509Store.h
@@ -1,10 +1,10 @@
-/**
- * Copyright 2021 BrutalWizard (https://github.com/bru74lw1z4rd). All Rights Reserved.
+/*
+ * Copyright 2023 BrutalWizard (https://github.com/bru74lw1z4rd). All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License"). You may not use
  * this file except in compliance with the License. You can obtain a copy
  * in the file LICENSE in the source distribution
- **/
+ */
 
 #ifndef QX509STORE_H
 #define QX509STORE_H
--- src/sources/QX509Store.cpp
+++ src/sources/QX509Store.cpp
@@ -1,13 +1,12 @@
-/**
- * Copyright 2021 BrutalWizard (https://github.com/bru74lw1z4rd). All Rights Reserved.
+/*
+ * Copyright 2023 BrutalWizard (https://github.com/bru74lw1z4rd). All Rights Reserved.
  *
  * Licensed under the Apache License 2.0 (the "License"). You may not use
  * this file except in compliance with the License. You can obtain a copy
  * in the file LICENSE in the source distribution
- **/
+ */
 
 #include "include/QX509Store.h"
-#include <stdexcept>
 
 QSimpleCrypto::QX509Store::QX509Store()
 {
